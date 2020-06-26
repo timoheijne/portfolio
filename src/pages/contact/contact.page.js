ce7acd1104ae4d218324293ef65778ae0f0628f1
Timo Heijne <me@timoheijne.nl>
2020-06-26 16:18:01 +0200

    Add contact page

--- src/pages/contact/contact.page.js
+++ src/pages/contact/contact.page.js
@@ -1,9 +1,25 @@
 import React from "react";
 
+import "./contact.page.scss"
+
+import {
+    Link
+} from "react-router-dom";
+
 export default class ContactPage extends React.Component {
     render() {
         return (
-        <h2>Contact Page</h2>
+            <div id="contact-page">
+                <div><h1 id="page-header">Contact Page</h1></div>
+
+                
+                <div id="contact-buttons">
+                    <a target="_blank" href="https://github.com/timoheijne">Github</a>
+                    <a target="_blank" href="https://www.linkedin.com/in/timoheijne/">LinkedIn</a>
+                    <a target="_blank" href="https://keybase.io/TimoHeijne">Keybase</a>
+                    <a href="mailto:me@timoheijne.nl">Email (me@timoheijne.nl)</a>
+                </div>
+            </div>
         );
     }
 }
