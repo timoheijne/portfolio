369f146c9ac664cd61a389fdf81de3b24ed0a750
Timo Heijne <me@timoheijne.nl>
2020-06-26 17:06:10 +0200

    Add project detail page layout

--- src/pages/project/project.page.js
+++ src/pages/project/project.page.js
@@ -1,12 +1,50 @@
 import React from "react";
 
 import "./project.page.scss"
+import ReactMarkdown from 'react-markdown/with-html'
+
+const input = '## Test <hr>'
 
 export default class ProjectPage extends React.Component {
     render() {
         return (
             <div id="project-page">
-                <div><h1 id="page-header">Project Page</h1></div>
+                <h1 id="page-header">NAME Overview</h1>
+
+                <div id="project-root">
+                    <div id="project-info">
+                        {/* IMAGES */}
+                        <div id="project-carousel">
+                            <img src="https://via.placeholder.com/1920x1080" alt="Project"/>
+                        </div>
+                        
+                        <div id="project-sections">
+                            <div class="project-section">
+                                <ReactMarkdown escapeHtml={false} source={input} />
+                            </div>
+                        </div>
+                    </div>
+
+                    <div id="project-sidebar">
+                        <ul>
+                            <li>Link Item</li>
+                            <li>Link Item</li>
+                            <li>Link Item</li>
+                            <li>Link Item</li>
+                            <li>Link Item</li>
+                        </ul>
+
+                        <ul>
+                            <li>Tech Item</li>
+                            <li>Tech Item</li>
+                            <li>Tech Item</li>
+                            <li>Tech Item</li>
+                            <li>Tech Item</li>
+                            <li>Tech Item</li>
+                            <li>Tech Item</li>
+                        </ul>
+                    </div>    
+                </div>
 
             </div>
         );
