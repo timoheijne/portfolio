e7443480a0c72bcff67a7ce6cc17fc0aaeee4511
Timo Heijne <me@timoheijne.nl>
2020-06-26 22:38:59 +0200

    Load projects based on manifest json

--- src/pages/projects/projects.page.js
+++ src/pages/projects/projects.page.js
@@ -4,16 +4,59 @@ import "./projects.page.scss"
 import ProjectComponent from "../../components/project/project.component";
 
 export default class ProjectsPage extends React.Component {
+    projects = [];
+
+    constructor() {
+        super()
+
+        this.state = {
+            projects: []
+        }
+    }
+
+    componentDidMount() {
+        fetch("/projects/manifest.json")
+        .then(response => response.json())
+        .then(jsonData => {
+
+            jsonData["projects"].forEach(project => {
+                fetch(`/projects/${project}.json`)
+                .then(response => response.json())
+                .then(projectData => { 
+                    
+                    this.projects.push(projectData)
+                    this.setState({
+                        projects: this.projects
+                    })
+                }).catch(error => {
+                    console.error(error)
+                })
+            });
+
+        }).catch(error => {
+            console.error(error)
+        })
+    }
+
+    getProjectList() {
+        let list = [];
+        this.state.projects.forEach(project => {
+            console.log(project)
+            list.push(<ProjectComponent project={project} />)
+        });
+
+        console.log(list)
+
+        return list
+    }
+
     render() {
+        console.log(this.projects)
         return (
             <div id="projects-page">
                 <div><h1 id="page-header">Projects Page</h1></div>
-
-                <ProjectComponent />
-                <ProjectComponent />
-                <ProjectComponent />
-                <ProjectComponent />
-                <ProjectComponent />
+                
+                {this.getProjectList()}
             </div>
         );
     }
