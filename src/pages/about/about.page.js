0871a856669c03abd85a7504bd4f3f0eb55ad3fd
Timo Heijne <me@timoheijne.nl>
2020-06-26 23:17:59 +0200

    Fix about me page scss

--- src/pages/about/about.page.js
+++ src/pages/about/about.page.js
@@ -13,7 +13,7 @@ export default class AboutPage extends React.Component {
                         <img src="/images/about-me.jpg" alt="Timo Heijne" />
                     </div>
 
-                    <div  id="about-text">
+                    <div id="about-text">
                     Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer sit amet urna aliquet, tincidunt augue quis, porttitor tortor. In eu finibus lorem. Integer auctor dignissim volutpat. Suspendisse potenti. Nullam sagittis urna laoreet ex aliquam, egestas egestas libero luctus. Cras nisi leo, sagittis at mauris eu, fringilla condimentum est. In cursus arcu ac nulla lobortis, sed tempus velit consequat. Praesent sed sapien at erat dapibus varius. Quisque eleifend sed dolor quis pharetra.
 
                     Quisque dolor risus, pellentesque a risus vel, ullamcorper dapibus augue. In orci orci, fringilla non magna in, mollis vulputate justo. Nunc eget odio vitae lectus mollis pretium quis eget erat. Aenean a dignissim arcu. Suspendisse quis dui vitae ipsum sollicitudin sollicitudin. Sed ultricies augue quis turpis imperdiet dignissim. Nullam lorem lorem, vulputate rhoncus ullamcorper at, vulputate nec mi. Donec sed gravida sem. Mauris lacus orci, aliquam non est in, suscipit iaculis urna.
